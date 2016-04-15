83731c47d94c3007fc369d3431604bc1628b784d
Guido Arnau <guido.arnau@gmail.com>
2016-04-16 00:10:51 +0100

    Build functions to predict whole year

--- mlp/TimeSeriesNnet.py
+++ mlp/TimeSeriesNnet.py
@@ -7,67 +7,87 @@ from sklearn.preprocessing import StandardScaler
 
 
 class TimeSeriesNnet(object):
-	def __init__(self, hidden_layers = [20, 15, 5], activation_functions = ['relu', 'relu', 'relu'], 
+    def __init__(self, hidden_layers = [20, 15, 5], activation_functions = ['relu', 'relu', 'relu'], 
               optimizer = SGD(), loss = 'mean_absolute_error'):
-		self.hidden_layers = hidden_layers
-		self.activation_functions = activation_functions
-		self.optimizer = optimizer
-		self.loss = loss
+        self.hidden_layers = hidden_layers
+        self.activation_functions = activation_functions
+        self.optimizer = optimizer
+        self.loss = loss
 
-		if len(self.hidden_layers) != len(self.activation_functions):
-			raise Exception("hidden_layers size must match activation_functions size")
+        if len(self.hidden_layers) != len(self.activation_functions):
+            raise Exception("hidden_layers size must match activation_functions size")
 
-	def fit(self, timeseries, lag = 7, epochs = 10000, verbose = 0):
-		self.timeseries = np.array(timeseries, dtype = "float64") # Apply log transformation por variance stationarity
-		self.lag = lag
-		self.n = len(timeseries)
-		if self.lag >= self.n:
-			raise ValueError("Lag is higher than length of the timeseries")
-		self.X = np.zeros((self.n - self.lag, self.lag), dtype = "float64")
-		self.y = np.log(self.timeseries[self.lag:])
-		self.epochs = epochs
-		self.scaler = StandardScaler()
-		self.verbose = verbose
+    def fit(self, timeseries, lag = 7, epochs = 10000, verbose = 0):
+        self.timeseries = np.array(timeseries, dtype = "float64") # Apply log transformation por variance stationarity
+        self.lag = lag
+        self.n = len(timeseries)
+        if self.lag >= self.n:
+            raise ValueError("Lag is higher than length of the timeseries")
+        self.X = np.zeros((self.n - self.lag, self.lag), dtype = "float64")
+        self.y = np.log(self.timeseries[self.lag:])
+        self.epochs = epochs
+        self.scaler = StandardScaler()
+        self.verbose = verbose
 
-		print "Building regressor matrix"
-		# Building X matrix
-		for i in range(0, self.n - lag):
-			self.X[i, :] = self.timeseries[range(i, i + lag)]
+        print "Building regressor matrix"
+        # Building X matrix
+        for i in range(0, self.n - lag):
+            self.X[i, :] = self.timeseries[range(i, i + lag)]
 
-		print "Scaling data"
-		self.scaler.fit(self.X)
-		self.X = self.scaler.transform(self.X)
+        print "Scaling data"
+        self.scaler.fit(self.X)
+        self.X = self.scaler.transform(self.X)
 
-		print "Checking network consistency"
-		# Neural net architecture
-		self.nn = Sequential()
-		self.nn.add(Dense(self.hidden_layers[0], input_shape = (self.X.shape[1],)))
-		self.nn.add(Activation(self.activation_functions[0]))
+        print "Checking network consistency"
+        # Neural net architecture
+        self.nn = Sequential()
+        self.nn.add(Dense(self.hidden_layers[0], input_shape = (self.X.shape[1],)))
+        self.nn.add(Activation(self.activation_functions[0]))
 
-		for layer_size, activation_function in zip(self.hidden_layers[1:],self.activation_functions[1:]):
-			self.nn.add(Dense(layer_size))
-			self.nn.add(Activation(activation_function))
+        for layer_size, activation_function in zip(self.hidden_layers[1:],self.activation_functions[1:]):
+            self.nn.add(Dense(layer_size))
+            self.nn.add(Activation(activation_function))
 
-		# Add final node
-		self.nn.add(Dense(1))
-		self.nn.add(Activation('linear'))
-		self.nn.compile(loss = self.loss, optimizer = self.optimizer)
+        # Add final node
+        self.nn.add(Dense(1))
+        self.nn.add(Activation('linear'))
+        self.nn.compile(loss = self.loss, optimizer = self.optimizer)
 
-		print "Training neural net"
-		# Train neural net
-		self.nn.fit(self.X, self.y, nb_epoch = self.epochs, verbose = self.verbose)
+        print "Training neural net"
+        # Train neural net
+        self.nn.fit(self.X, self.y, nb_epoch = self.epochs, verbose = self.verbose, validation_split=0.1)
 
-	def predict_ahead(self, n_ahead = 1):
-		# Store predictions and predict iteratively
-		self.predictions = np.zeros(n_ahead)
+    def predict_ahead(self, n_ahead = 1):
+        # Store predictions and predict iteratively
+        self.predictions = np.zeros(n_ahead)
 
-		for i in range(n_ahead):
-			self.current_x = self.timeseries[-self.lag:]
-			self.current_x = self.current_x.reshape((1, self.lag))
-			self.current_x = self.scaler.transform(self.current_x)
-			self.next_pred = self.nn.predict(self.current_x)
-			self.predictions[i] = np.exp(self.next_pred[0, 0])
-			self.timeseries = np.concatenate((self.timeseries, np.exp(self.next_pred[0,:])), axis = 0)
+        for i in range(n_ahead):
+            self.current_x = self.timeseries[-self.lag:]
+            self.current_x = self.current_x.reshape((1, self.lag))
+            self.current_x = self.scaler.transform(self.current_x)
+            self.next_pred = self.nn.predict(self.current_x)
+            self.predictions[i] = np.exp(self.next_pred[0, 0])
+            self.timeseries = np.concatenate((self.timeseries, np.exp(self.next_pred[0,:])), axis = 0)
 
-		return self.predictions
+        return self.predictions
+
+    def predict_year(self, inp):
+        print 'Predicting values'
+
+        self.predictions = np.zeros(len(inp))
+        inp = np.array(inp, dtype='float64')
+        tempX = np.zeros((len(inp) - self.lag, self.lag), dtype = "float64")
+
+        for i in range(0, len(inp) - self.lag):
+            tempX[i, :] = inp[range(i, i + self.lag)]
+
+        self.scaler.fit(tempX)
+        tempX = self.scaler.transform(tempX)
+
+        for i in xrange(tempX.shape[0]):
+            self.current_x = tempX[i,:].reshape((1, self.lag))
+            self.next_pred = self.nn.predict(self.current_x)
+            self.predictions[i] = np.exp(self.next_pred[0, 0])
+
+        return self.predictions
 
--- mlp/mlp_run.py
+++ mlp/mlp_run.py
@@ -6,7 +6,10 @@ from sklearn.metrics import mean_squared_error
 
 # read csv and separate data
 csv = numpy.genfromtxt('../datasets/2007.txt', delimiter=";", names=True, dtype=None, missing_values='?')
-time_series = map(lambda row : 0 if numpy.isnan(row[2]) else row[2], filter(lambda row: row[0].rpartition('/')[2] == '2007', csv))
+time_series = map(lambda row : 0 if numpy.isnan(row[2]) else row[2]*1000, filter(lambda row: row[0].rpartition('/')[2] == '2007', csv))
+
+csv = numpy.genfromtxt('../datasets/2008.txt', delimiter=";", names=True, dtype=None, missing_values='?')
+test_series = map(lambda row : 0 if numpy.isnan(row[2]) else row[2]*1000, filter(lambda row: row[0].rpartition('/')[2] == '2008', csv))
 
 def collapse (ts, ahead):
     r = []
@@ -14,22 +17,22 @@ def collapse (ts, ahead):
         r.append(max(ts[i:i+ahead-1]))
     return r
 
-nahead = 24
-time_series = collapse(time_series,nahead)
+hours = 60
+time_series = collapse(time_series,hours)
+test_series = collapse(test_series,hours)
 
 time_series = np.array(time_series,dtype='int32')
-trainset, testset = time_series[0:-nahead], time_series[-nahead:]
 
 neural_net = TimeSeriesNnet(hidden_layers = [20, 15, 5], activation_functions = ['sigmoid', 'sigmoid', 'sigmoid'])
-neural_net.fit(trainset, lag = 1, epochs = 1000)
-neural_net.predict_ahead(n_ahead = nahead)
+neural_net.fit(time_series, lag=4, epochs=500)
+neural_net.predict_year(test_series)
 
 # RMSE Training error
-mse = mean_squared_error(testset, neural_net.timeseries[-nahead:])
+mse = mean_squared_error(test_series, neural_net.predictions)
 print numpy.sqrt(mse)
 
-plt.plot(range(len(neural_net.timeseries)), neural_net.timeseries, '-r', label='Predictions', linewidth=1)
-plt.plot(range(len(time_series)), time_series, '-g',  label='Original series')
+plt.plot(range(len(neural_net.predictions)), neural_net.predictions, '-r', label='Predictions', linewidth=1)
+plt.plot(range(len(test_series)), test_series, '-g',  label='Original series')
 plt.title("")
 plt.xlabel("")
 plt.ylabel("")
